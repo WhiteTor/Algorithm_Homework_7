5639bf931e21432e26e8f337c33df9dc9c7c5fcf
WhiteTor <kirito10@naver.com>
2022-12-05 02:35:56 +0900

    upload, in process..

---
+++ fractional.py
@@ -0,0 +1,26 @@
+def fractional():
+    class treasure:
+        def __init__(self, value, weight):
+            self.value = value
+            self.weight = weight
+            self.effective = value / weight
+
+    K = int(input())   #배낭의 크기
+    n = int(input())   #아이템의 갯수
+    arr_weight = list(map(int, input().split()))   #아이템의 크기
+    arr_value = list(map(int, input().split()))   #아이템의 가치
+    arr_treasure = []
+
+    for i in range(n):
+        arr_treasure.append(treasure(arr_value[i], arr_weight[i]))
+
+    arr_treasure.sort(key = lambda x : -x.effective)
+
+    while(-1):
+        result = 0
+        temp = 0
+        for i in range(n):
+            if temp + arr_treasure[i].weight > W:
+                break
+        temp += arr_treasure[i].weight
+        result += arr_treasure[i].value
